6ab67565337c06a0783275a1b6fd46023f9cb43f
sichper241 <122784598+sichper241@users.noreply.github.com>
2023-01-16 11:16:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
+let aleatorio = 0
+input.onButtonPressed(Button.A, function () {
+    aleatorio = randint(1, 3)
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    if (aleatorio == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (aleatorio == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (aleatorio == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
 basic.forever(function () {
 	
 })
